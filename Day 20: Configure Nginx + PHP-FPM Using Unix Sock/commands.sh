799636c3d41472d692ebdc278cb7d238cfc75334
grauziitisos <85872382+grauziitisos@users.noreply.github.com>
2026-03-10 20:46:28 +0200

    Update commands.sh

--- Day 20: Configure Nginx + PHP-FPM Using Unix Sock/commands.sh
+++ Day 20: Configure Nginx + PHP-FPM Using Unix Sock/commands.sh
@@ -1,4 +1,4 @@
-sudo dnf install ansible -y
+sudo dnf install ansible epel-release -y
 nano inventory.ini
 nano playbook.yaml
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory.ini playbook.yaml
